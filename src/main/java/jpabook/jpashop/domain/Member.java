60859f2f25b22965ba60d9e0204fd31380868dd8
sangwookShin <shinsw6455@naver.com>
2021-10-15 23:03:33 +0900

    엔티티 클래스 생성

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+//    mappedBy 쓰면 member에 의해 매핑된 것
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
